251624d72327af7f42bde1380142645be93ae1f5
breakyuna <18855640756wcy@gmail.com>
2026-09-16 17:38:59 +0800

    fix: import Surface in HomeTab

--- app/src/main/java/com/breakyuna/esjzone/ui/tab/HomeTab.kt
+++ app/src/main/java/com/breakyuna/esjzone/ui/tab/HomeTab.kt
@@ -48,6 +48,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
